84df8f6da94ae8cc7db99bc6f8f78822bee69d97
Baobax <keith@indvstri.com>
2022-04-28 23:11:43 +0200

    modified layout/default.vue and pages/index.vue, added components/TheNavigation.vue and pages/_slug.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,28 @@
 <template>
   <v-app>
+    <!-- Navigation -->
+    <TheNavigation />
     <v-main>
-	  <v-container>
-	    <Nuxt />
-	  </v-container>
-	</v-main>
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="10">
+            <Nuxt />
+          </v-col>
+          <v-col cols="12" md="2">
+            <!-- Ads/side content -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
   </v-app>
 </template>
 
 <script>
-export default {}
+import TheNavigation from '~/components/TheNavigation.vue'
+export default {
+  name: 'DefaultLayout',
+  components: { TheNavigation },
+}
 </script>
 
 <style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,59 @@
 <template>
   <div>
-    <h4>Welcome To The NuxtBlog</h4>
+    <v-container fluid>
+      <div class="intro mt-5 mb-8">
+        <h1 class="text-h1">NuxtBlog</h1>
+        <h2 class="mt-2">100x Your Nuxt Skills 🚀</h2>
+      </div>
+      <v-row class="posts-container">
+        <v-col cols="12">
+          <div class="filter">
+            <v-select
+              v-model="category"
+              style="width: 120px"
+              outlined
+              dense
+              hide-details="auto"
+              :items="['All', 'Coding', 'Youtube']"
+            />
+          </div>
+        </v-col>
+        <v-col v-for="post in 4" :key="post" cols="12" md="6">
+          <v-card elevation="0">
+            <v-card-title>My First Blog</v-card-title>
+            <v-card-text
+              >Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt
+              ratione quam quia eveniet repellendus inventore sint eius possimus
+              debitis blanditiis.</v-card-text
+            >
+            <v-card-actions>
+              <v-btn text to="/1">Read More</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+      <v-row class="post-pagination">
+        <v-col cols="12" class="text-right">
+          <v-btn>
+            <v-icon small>mdi-arrow-left</v-icon>
+            Prev.
+          </v-btn>
+          <v-btn>
+            <v-icon small>mdi-arrow-right</v-icon>
+            Next
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage',
+  name: 'HomePage',
+  layout: 'DefaultLayout',
+  data: () => ({
+    category: 'All',
+  }),
 }
 </script>
